f73483ec9346c4d16b3bf9ff9bc81feec708af29
muhammedashraf04 <allammohamed303@gmail.com>
2025-10-31 22:21:54 +0200

    editData

---
+++ Backend/ECommerceWeb.Models/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerceWeb.Models.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Vendor_ID { get; set; }
+        public int Category_ID { get; set; }
+        public decimal Discount { get; set; }
+        public decimal PriceDiscount { get; set; }
+        public int Quantity { get; set; }
+        public decimal ratings { get; set; }
+    }
+}
