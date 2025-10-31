76182f1f0c82018e6e4fc49d1effcc0f82830110
omar emad <omaremad11113@gmail.com>
2025-11-01 00:38:49 +0200

    Made the Models for the database

---
+++ Backend/ECommerceWeb.Models/Models/CartItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ECommerceWeb.Models.Models.BaseModels;
+
+namespace ECommerceWeb.Models.Models
+{
+    public class CartItem : BaseItem
+    {
+        [Required]
+        public int CartId { get; set; }
+        public virtual Cart? Cart { get; set; }
+    }
+}
