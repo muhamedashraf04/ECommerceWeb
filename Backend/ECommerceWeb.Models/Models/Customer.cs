9d9e3a6e2bb9ea94745ff63d22219e8b81e0a399
omar emad <omaremad11113@gmail.com>
2025-11-01 02:46:49 +0200

    made some dummy data and made some edits in the relations

--- Backend/ECommerceWeb.Models/Models/Customer.cs
+++ Backend/ECommerceWeb.Models/Models/Customer.cs
@@ -2,5 +2,7 @@
 {
     public class Customer : BaseModels.BaseUser
     {
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public Cart? Cart { get; set; }
     }
 }
