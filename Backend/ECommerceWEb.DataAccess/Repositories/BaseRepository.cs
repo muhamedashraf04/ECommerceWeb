72cbd5e0ca5c0dc7c5f8a90cf0fb8cdb41fa8327
omar emad <omaremad11113@gmail.com>
2025-11-04 06:12:12 +0200

    made all the repo's

--- Backend/ECommerceWEb.DataAccess/Repositories/BaseRepository.cs
+++ Backend/ECommerceWEb.DataAccess/Repositories/BaseRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace ECommerceWeb.DataAccess.Interfaces
+﻿using System.Linq.Expressions;
+using ECommerceWeb.DataAccess.Repositories.Interfaces;
+namespace ECommerceWeb.DataAccess.Repositories
 {
-    public interface IBaseRepository : IBaseRepository<T>
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
+        public void Create(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(T Obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> Filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetAsync(Expression<Func<bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
