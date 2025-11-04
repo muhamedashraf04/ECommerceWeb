fa9966909c283b31044fec6878017e23a9ff5845
omar emad <omaremad11113@gmail.com>
2025-11-04 06:13:29 +0200

    removed some mistakes in the repo's

--- Backend/ECommerceWEb.DataAccess/Repositories/CustomerRepository.cs
+++ Backend/ECommerceWEb.DataAccess/Repositories/CustomerRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ECommerceWeb.DataAccess.Repositories;
-using ECommerceWeb.Models.Models;
+﻿using ECommerceWeb.Models.Models;
 
 namespace ECommerceWeb.DataAccess.Repositories
 {
