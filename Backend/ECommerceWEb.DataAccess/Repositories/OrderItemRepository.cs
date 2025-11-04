8314bb87aa1d5d22fc8535d07d65d63f10a17a07
omar emad <omaremad11113@gmail.com>
2025-11-04 06:12:12 +0200

    made all the repo's

--- Backend/ECommerceWEb.DataAccess/Repositories/OrderItemRepository.cs
+++ Backend/ECommerceWEb.DataAccess/Repositories/OrderItemRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ECommerceWeb.Models.Models;
 
-namespace ECommerceWeb.DataAccess.Interfaces
+namespace ECommerceWeb.DataAccess.Repositories
 {
-    internal class IOrderItemRepository
+    public class IOrderItemRepository : BaseRepository<OrderItem>
     {
     }
 }
