05e770a9225e226551947705883641c31408dcc5
omar emad <omaremad11113@gmail.com>
2025-11-04 06:04:42 +0200

    made the IBaseRepo and made setup for all interfaces

---
+++ Backend/ECommerceWEb.DataAccess/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ECommerceWeb.DataAccess.Repositories.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T Get(Expression<Func<T, bool>> Filter);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null);
+        void Create(T obj);
+        bool Remove(int id);
+        void Edit(T Obj);
+        Task GetAsync(Expression<Func<bool>> filter);
+        Task<IEnumerable<T>> GetAllAsync();
+
+    }
+}
+
---
+++ Backend/ECommerceWEb.DataAccess/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿using ECommerceWeb.Models.Models;
+namespace ECommerceWeb.DataAccess.Repositories.Interfaces
+{
+    public interface IProductRepository : IBaseRepository<Product>
+    {
+    }
+}
