12999c0b8ed837fc2344f89e5aff21c4d3939188
omar emad <omaremad11113@gmail.com>
2025-11-04 09:45:48 +0200

    BaseRepo done for now

--- Backend/ECommerceWEb.DataAccess/Repositories/Interfaces/IBaseRepository.cs
+++ Backend/ECommerceWEb.DataAccess/Repositories/Interfaces/IBaseRepository.cs
@@ -4,11 +4,11 @@ namespace ECommerceWeb.DataAccess.Repositories.Interfaces
 {
     public interface IBaseRepository<T> where T : class
     {
-        Task<T> GetAsync(Expression<Func<T, bool>> Filter);
+        Task<T?> GetAsync(Expression<Func<T, bool>> Filter);
         Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
-        Task<T> CreateAsync(T obj);
+        Task<bool> CreateAsync(T obj);
         Task<bool> RemoveAsync(int id);
-        Task EditAsync(T Obj);
+        Task<bool> EditAsync(T Obj);
         Task<IEnumerable<T>> GetAllAsync();
 
     }
