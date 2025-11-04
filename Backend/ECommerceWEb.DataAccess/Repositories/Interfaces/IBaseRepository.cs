4fc8dd9975594245bfbbc022baac73505758790d
omar emad <omaremad11113@gmail.com>
2025-11-04 06:34:05 +0200

    setup for base repo and changes in IBaserepo going ro change the functions

--- Backend/ECommerceWEb.DataAccess/Repositories/Interfaces/IBaseRepository.cs
+++ Backend/ECommerceWEb.DataAccess/Repositories/Interfaces/IBaseRepository.cs
@@ -4,12 +4,11 @@ namespace ECommerceWeb.DataAccess.Repositories.Interfaces
 {
     public interface IBaseRepository<T> where T : class
     {
-        T Get(Expression<Func<T, bool>> Filter);
-        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null);
-        void Create(T obj);
-        bool Remove(int id);
-        void Edit(T Obj);
-        Task GetAsync(Expression<Func<bool>> filter);
+        Task<T> GetAsync(Expression<Func<T, bool>> Filter);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> CreateAsync(T obj);
+        Task<bool> RemoveAsync(int id);
+        Task EditAsync(T Obj);
         Task<IEnumerable<T>> GetAllAsync();
 
     }
