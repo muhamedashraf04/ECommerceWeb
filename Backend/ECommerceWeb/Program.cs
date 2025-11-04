3d15e577263ff3b9ac2e3241801f82e7dc989f00
muhammedashraf04 <allammohamed303@gmail.com>
2025-11-04 20:40:37 +0200

    Adding Scalar to the project

--- Backend/ECommerceWeb/Program.cs
+++ Backend/ECommerceWeb/Program.cs
@@ -1,5 +1,6 @@
 using ECommerceWeb.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
