97c7951b9db4e279b4309e566eaa93488848b1b8
muhammedashraf04 <allammohamed303@gmail.com>
2025-11-05 22:53:48 +0200

    Authurization Completed

---
+++ Backend/ECommerceWeb.DataAccess/Migrations/20251105175530_Adding Roles.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceWeb.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Vendor");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Customer");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Vendor",
+                newName: "PasswordHash");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Vendor",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Customer",
+                newName: "PasswordHash");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Customer",
+                newName: "Name");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Vendor",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customer",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Name", "Role" },
+                values: new object[] { "Omar Shoulkamy", null });
+
+            migrationBuilder.UpdateData(
+                table: "Vendor",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Name", "Role" },
+                values: new object[] { "John Vendor", null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Vendor");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Customer");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Vendor",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Vendor",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Customer",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Customer",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Vendor",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Customer",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FirstName", "LastName" },
+                values: new object[] { "Omar", "Shoulkamy" });
+
+            migrationBuilder.UpdateData(
+                table: "Vendor",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FirstName", "LastName" },
+                values: new object[] { "John", "Vendor" });
+        }
+    }
+}
--- Backend/ECommerceWeb.Models/DTOs/UserLoginDTO.cs
+++ Backend/ECommerceWeb.Models/DTOs/UserLoginDTO.cs
@@ -2,12 +2,12 @@
 
 namespace ECommerceWeb.Models.DTOs
 {
-    public class UserDTO
+    public class UserLoginDTO
     {
-        public int Id { get; set; }
         [Required]
-        public string? Name { get; set; }
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
         public string? Password { get; set; }
-
     }
 }
