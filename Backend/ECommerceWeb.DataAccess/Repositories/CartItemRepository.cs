7eee670e89322f8d8a53dfac4b3c2744388cc1b3
muhamedashraf04 <allammohamed303@gmail.com>
2025-12-03 20:08:52 +0200

    Category Controller

---
+++ Backend/ECommerceWeb.DataAccess/Repositories/CartItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceWeb.DataAccess.Data;
+using ECommerceWeb.Models.Models;
+
+namespace ECommerceWeb.DataAccess.Repositories
+{
+    public class CartItemRepository : BaseRepository<CartItem>
+    {
+        ApplicationDbContext _dbContext;
+
+        public CartItemRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
---
+++ Backend/ECommerceWeb.DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceWeb.DataAccess.Data;
+using ECommerceWeb.DataAccess.Repositories.Interfaces;
+using ECommerceWeb.Models.Models;
+
+namespace ECommerceWeb.DataAccess.Repositories
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        ApplicationDbContext _dbContext;
+        public OrderRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
