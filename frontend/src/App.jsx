c4a0d5d282328f62740f2ba5bca0be55de081f5e
SamaSadek <Samaabdelsadek@gmail.com>
2025-12-08 19:09:42 +0200

    ProductPage ready for backend connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 import CartPage from './pages/CartPage';
+import ProductPage from './pages/ProductPage'
 import MainLayout from './layouts/MainLayout';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Navigate to="/auth" />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/prod" element={<ProductPage />} />
       </Routes>
     </MainLayout>
   );
