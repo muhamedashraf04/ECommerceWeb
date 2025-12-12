918981122339c8b8f31143fb09e57b1b5cc68c40
omar emad <omaremad11113@gmail.com>
2025-12-13 01:49:36 +0200

    Made the Order Controller complete

---
+++ Backend/ECommerceWeb.Application/DTOs/OrderDTOs/OrderDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceWeb.Application.DTOs.OrderDTOs
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public string OrderStatus { get; set; }
+        public List<OrderItemDto> Items { get; set; } = new();
+    }
+}
