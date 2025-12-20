3e9627414dac4d0c1ed0dbbd13d7be3867ae2aa3
OmarKhalafallah <mr.omar.waleed2002@gmail.com>
2025-12-20 03:24:10 +0200

    Fixed some errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      // Any request starting with /api will be forwarded to Azure
       '/api': {
-        target: 'https://deployment.mangoisland-dd0744d7.italynorth.azurecontainerapps.io', // Your Docker Backend Port
+        target: 'https://deployment.mangoisland-dd0744d7.italynorth.azurecontainerapps.io/',
         changeOrigin: true,
         secure: false,
-      },
-    },
-  },
+      }
+    }
+  }
 })
