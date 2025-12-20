797bb446acbdba061060311255dc0a9e60c0a5e9
OmarKhalafallah <mr.omar.waleed2002@gmail.com>
2025-12-20 03:24:10 +0200

    Fixed some errors

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,25 +1,8 @@
-// frontend/src/api/axiosConfig.js
 import axios from 'axios';
 
-// The URL from your api-docs.html
-const BASE_URL = 'https://deployment.mangoisland-dd0744d7.italynorth.azurecontainerapps.io'; 
-
-const api = axios.create({
-    baseURL: BASE_URL,
+export default axios.create({
+    baseURL: '', // LEAVE THIS EMPTY. It will automatically default to the proxy.
     headers: {
-        'Content-Type': 'application/json',
-    },
-});
-
-// Automatically add the Token if the user is logged in
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+        "Content-type": "application/json"
     }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
 });
-
-export default api;
